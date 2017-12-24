5482f73be3a260bacb00e0cf7fd69822ef854a86
yamada <yamada@192.168.0.10>
2017-12-24 10:53:54 +0900

    init

---
+++ src/main/java/org/yip/sandbox/SandboxMain.java
@@ -0,0 +1,16 @@
+package org.yip.sandbox;
+
+/**
+ * SandboxMain.
+ */
+public class SandboxMain{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args){
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
