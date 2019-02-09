952c33c99df2248eb2f41c4a8e8723b58fd6292d
yipuran <postyip2@gmail.com>
2019-02-09 22:22:01 +0900

    test

--- src/main/java/org/yip/sandbox/RankTest.java
+++ src/main/java/org/yip/sandbox/RankTest.java
@@ -1,9 +1,6 @@
 package org.yip.sandbox;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.yipuran.rank.RankAccumulator;
+//import org.yipuran.rank.RankAccumulator;
 
 /**
  * RankTest1
@@ -11,23 +8,23 @@ import org.yipuran.rank.RankAccumulator;
 public class RankTest{
 	public static void main(String[] args){
 
-		List<User> list1 = UserFactory.creeate3().stream()
-		.sorted((a, b)->Integer.valueOf(b.point).compareTo(Integer.valueOf(a.point)))
-		.collect(()->new ArrayList<User>(), RankAccumulator.seek(t->t.point, (t, u)->t.rank=u), (r, u)->{});
-
-		System.out.println("======= seek ===========");
-		list1.stream().forEach(e->{
-			System.out.println(e.name + ":" + String.format("%5d", e.point) + " : " + e.rank );
-		});
-
-		List<User> list2 = UserFactory.creeate3().stream()
-		.sorted((a, b)->Integer.valueOf(b.point).compareTo(Integer.valueOf(a.point)))
-		.collect(()->new ArrayList<User>(), RankAccumulator.skip(t->t.point, (t, u)->t.rank=u), (r, u)->{});
-
-		System.out.println("======= skip  ===========");
-		list2.stream().forEach(e->{
-			System.out.println(e.name + ":" + String.format("%5d", e.point) + " : " + e.rank );
-		});
+//		List<User> list1 = UserFactory.creeate3().stream()
+//		.sorted((a, b)->Integer.valueOf(b.point).compareTo(Integer.valueOf(a.point)))
+//		.collect(()->new ArrayList<User>(), RankAccumulator.seek(t->t.point, (t, u)->t.rank=u), (r, u)->{});
+//
+//		System.out.println("======= seek ===========");
+//		list1.stream().forEach(e->{
+//			System.out.println(e.name + ":" + String.format("%5d", e.point) + " : " + e.rank );
+//		});
+//
+//		List<User> list2 = UserFactory.creeate3().stream()
+//		.sorted((a, b)->Integer.valueOf(b.point).compareTo(Integer.valueOf(a.point)))
+//		.collect(()->new ArrayList<User>(), RankAccumulator.skip(t->t.point, (t, u)->t.rank=u), (r, u)->{});
+//
+//		System.out.println("======= skip  ===========");
+//		list2.stream().forEach(e->{
+//			System.out.println(e.name + ":" + String.format("%5d", e.point) + " : " + e.rank );
+//		});
 
 	}
 }
--- src/main/java/org/yipuran/rank/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * 順位を決定する処理パッケージ.
- */
-package org.yipuran.rank;
