45831dcb1075e10745abaa0683281120b56f10fc
yipuran <postyip2@gmail.com>
2019-03-06 20:01:38 +0900

    no message

--- src/main/java/org/yip/sandbox/jsontest/develop/JsonDeserializeFunction.java
+++
@@ -1,9 +0,0 @@
-package org.yip.sandbox.jsontest.develop;
-
-/**
- * JsonDeserializeFunction :
- */
-@FunctionalInterface
-public interface JsonDeserializeFunction<JsonElement, T, JsonDeserializationContext, R>{
-	R apply(JsonElement je, T t, JsonDeserializationContext v);
-}
