4f16aa0816569588ab0439b6d6f8de74282cb70f
yipuran <postyip2@gmail.com>
2019-12-02 22:07:26 +0900

    no message

---
+++ src/main/java/org/yip/sandbox/fileio/TestIniFileParser.java
@@ -0,0 +1,36 @@
+package org.yip.sandbox.fileio;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.yip.sandbox.IniFileParser;
+
+
+/**
+ * TestIniFileParser.java
+ */
+public class TestIniFileParser{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args){
+		try(FileInputStream in = new FileInputStream(ClassLoader.getSystemClassLoader().getResource("sample.ini").getPath())){
+			IniFileParser iniParser = IniFileParser.read(in);
+
+			System.out.println("keys = [" + iniParser.getString("keys") + "]" );
+			System.out.println( "customer.name = [" + iniParser.getString("customer", "city") + "]" );
+			System.out.println( "customer.name = [" + iniParser.getString("customer", "name") + "]" );
+			System.out.println( "customer.master = [" + iniParser.getBoolean("customer", "master") + "]" );
+			System.out.println( "server.port = [" + iniParser.getInteger("server", "port").orElseThrow() + "]" );
+			System.out.println( "server.name = [" + iniParser.getString("server", "password") + "]" );
+			String json = iniParser.toJson();
+			System.out.println(json);
+
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+
+	}
+
+}
