207e2681d51526efa66aa45d2d33ba21af69c82a
yipuran <postyip2@gmail.com>
2020-05-19 21:39:41 +0900

    no message

---
+++ src/test/java/org/yip/sandbox/pch/FeatureRepeatablePermutationTest.java
@@ -0,0 +1,25 @@
+package org.yip.sandbox.pch;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+/**
+* FeatureRepeatablePermutationTest
+*/
+public class FeatureRepeatablePermutationTest{
+
+	@Test
+	public void testCount(){
+
+		FeatureRepeatablePermutation<String> f = FeatureRepeatablePermutation.of(Arrays.asList("A", "B", "C"));
+
+		int count = f.compute(3).size();
+
+		assertEquals(count, 27);
+
+	}
+
+}
